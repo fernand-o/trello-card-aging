a993000379bb942b1333ed9851ca24b3c6df3e8c
Fernando Almeida <fernandoprsbr@gmail.com>
2017-01-23 20:35:37 -0200

    Fix identation

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {  
- 
-  if ((changeInfo.title != undefined) && (changeInfo.title.includes('|'))){    
-    chrome.tabs.executeScript(tab.id, {code: "start_aging('"+ changeInfo.title +"');"});
-  }
-  
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+    if ((changeInfo.title != undefined) && (changeInfo.title.includes('|'))) {
+        chrome.tabs.executeScript(tab.id, {
+            code: "start_aging('" + changeInfo.title + "');"
+        });
+    }
+
 });
